616be32bbcdf327c0310f8f007eb07b499762329
Andrey Karpenko <dron.donbass@gmail.com>
2026-05-11 19:53:21 +0300

    dron-dev: cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,16 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://talkora-one.vercel.app/'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  })
+
   const config = new DocumentBuilder()
       .setTitle('Talkora API')
       .setDescription('API documentation: <a href="/v1/api-json" target="_blank">/v1/api-json</a>')
