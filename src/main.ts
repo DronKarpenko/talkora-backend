45305c96077cf8c35c097ed17fe0aabd29b578db
Andrey Karpenko <dron.donbass@gmail.com>
2026-05-11 18:04:57 +0300

    dron-dev: add json

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,14 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
       .setTitle('Talkora API')
-      .setDescription('API documentation')
+      .setDescription('API documentation: <a href="/v1/api-json" target="_blank">/v1/api-json</a>')
       .setVersion('1.0')
       .build()
 
   const document = SwaggerModule.createDocument(app, config)
 
   SwaggerModule.setup('api', app, document, {
-    jsonDocumentUrl: '/api-json',
+    jsonDocumentUrl: '/v1/api-json',
     explorer: true,
   })
 
